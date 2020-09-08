2d9ded64812cae0c984dab723d05cdb8eaad4104
ashishmainalee <ashishmainalee@gmail.com>
2020-09-08 16:04:54 +0545

    success

---
+++ src/page/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Direcrory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="conatiner">
+    <Direcrory />
+  </div>
+);
+
+export default HomePage;
